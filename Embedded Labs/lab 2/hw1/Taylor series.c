5b0f66660a3ad798c34a80174ab78b808a8d1ee3
Yasien Essam <yasien.essam99@gmail.com>
2022-04-01 22:04:52 +0200

    HW 1

--- Embedded Labs/lab 2/hw1/Taylor series.c
+++ Embedded Labs/lab 2/hw1/Taylor series.c
@@ -1,15 +1,11 @@
 #include<stdio.h> 
-#include <math.h>
 int main()    {    
     float x, itr_num = 0.0f;
-    int i ,fact = 1;
+    int i = 1;
     float result = 1.0f;
     printf("Enter the exponent and the number of iteration:");    
     scanf("%f %f",&x ,&itr_num);
-    for (int i = 1; i < itr_num ; i++)
-    {
-        fact = fact * i;
-        result = result + (pow(x,i) /fact);
-    }
+    for (i = itr_num - 1, result = 1; i > 0; --i )
+        result = 1 + x * result / i; 
     printf("exponential result : %0.5f",result);
 }    
