fd47236c3b7e15af6478382fcd8c6e0c52490c31
Yasien Essam <yasien.essam99@gmail.com>
2022-04-29 17:48:29 +0200

    add 1532

--- Leet Code/Easy/Count Odd Numbers in an Interval Range.py
+++
@@ -1,9 +0,0 @@
-import math
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        count = (high - low) / 2
-        #if both nums are odd add 1 to the count 
-        if (low%2 != 0) and (high%2 != 0):
-            count+=1
-        count = math.ceil(count)
-        return count
