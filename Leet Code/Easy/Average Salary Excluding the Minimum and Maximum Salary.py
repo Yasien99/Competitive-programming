fcd97bd31749444ae2453f3bfcb53e320c1c88d0
Yasien Essam <yasien.essam99@gmail.com>
2022-04-29 17:49:53 +0200

    add 1491

--- Leet Code/Easy/Average Salary Excluding the Minimum and Maximum Salary.py
+++
@@ -1,7 +0,0 @@
-import statistics
-class Solution:
-    def average(self, salary: List[int]) -> float:
-        salary.sort() 
-        del salary[0]
-        del salary[-1]
-        return statistics.mean(salary)
